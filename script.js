152eccea38366bfc50c3122f28b5ab41aa791872
JT <prejt@hotmail.com>
2022-09-25 19:34:22 -0700

    quote generator files

---
+++ script.js
@@ -0,0 +1,74 @@
+// get quotes from api
+
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+let apiQuotes = [];
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// show new quote
+function newQuote(){
+    loading();
+    //use math floor & math random methods to generate a random #, which has decimal points, then round it down/up to being a whole integer
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    //check if author is blank (null), replace w/ unknown
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    } else{
+        authorText.textContent = quote.author;
+    }
+    // check quote length, determines styling
+    if (quote.text.length > 100){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    // set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error){
+    
+        // catch error here 
+    }
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// tweet a quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// on load
+getQuotes();
